e32744243633557777b3d259502db7b3327a004e
Daniel Loyd <dhloyd@icloud.com>
2018-08-24 19:41:55 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,30 @@
-# Write your code here!
+def game_hash()
+  :home{
+    :team_name{}, 
+    :colors{},
+    :players{:number,
+             :shoe,
+             :points,
+             :rebounds,
+             :assists,
+             :steals,
+             :blocks,
+             :slam_dunks,
+}
+},
+  :home{
+    :team_name{}, 
+    :colors{},
+    :players{:number,
+             :shoe,
+             :points,
+             :rebounds,
+             :assists,
+             :steals,
+             :blocks,
+             :slam_dunks,
+}
+},
 
 
 
