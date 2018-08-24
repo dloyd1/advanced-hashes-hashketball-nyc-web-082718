4220eebbca3bfb672b1d50e4fe1bef268b4efc02
Daniel Loyd <dhloyd@icloud.com>
2018-08-24 20:02:11 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,26 +3,26 @@ def game_hash()
     :team_name => {"Brooklyn Nets"},
     :colors => {"Black", "White"},
     :players => {
-                  { :name => {["Alan Anderson", "Reggie Evangs", "Brook Lopez", "Mason Plumlee", "Jason Terry"]},
-                    :number => [0, 30, 11, 1, 31]
-                    :shoe => [16, 14, 17, 19, 15],
-                    :points => [22, 12, 17, 26, 19],
-                    :rebounds => [12, 12, 19, 12, 2],
-                    :assists => [12, 12, 10, 6, 2],
-                    :steals => [3, 12, 3, 3, 4],
-                    :blocks => [1, 12, 1, 8, 11],
-                    :slam_dunks => [1, 7, 15, 5, 1]
-                },
-                }
-},
+      :name => ["Alan Anderson", "Reggie Evangs", "Brook Lopez", "Mason Plumlee", "Jason Terry"],
+      :number => [0, 30, 11, 1, 31]
+      :shoe => [16, 14, 17, 19, 15],
+      :points => [22, 12, 17, 26, 19],
+      :rebounds => [12, 12, 19, 12, 2],
+      :assists => [12, 12, 10, 6, 2],
+      :steals => [3, 12, 3, 3, 4],
+      :blocks => [1, 12, 1, 8, 11],
+      :slam_dunks => [1, 7, 15, 5, 1]}},
   :home{
     :team_name => {"Charlotte Hornets"}
     :colors => {"Turquoise", "Purple"},
-    :players => {{name: , shoe: , points: , rebounds: , assists: , steals: , blockers: , slam_dunks:},
-                {name: , shoe: , points: , rebounds: , assists: , steals: , blockers: , slam_dunks:},
-                {name: , shoe: , points: , rebounds: , assists: , steals: , blockers: , slam_dunks:},
-                {name: , shoe: , points: , rebounds: , assists: , steals: , blockers: , slam_dunks:},
-                {name: , shoe: , points: , rebounds: , assists: , steals: , blockers: , slam_dunks:}}
-
-}
-},
+    :players => {
+      :name => ["Alan Anderson", "Reggie Evangs", "Brook Lopez", "Mason Plumlee", "Jason Terry"],
+      :number => [0, 30, 11, 1, 31]
+      :shoe => [16, 14, 17, 19, 15],
+      :points => [22, 12, 17, 26, 19],
+      :rebounds => [12, 12, 19, 12, 2],
+      :assists => [12, 12, 10, 6, 2],
+      :steals => [3, 12, 3, 3, 4],
+      :blocks => [1, 12, 1, 8, 11],
+      :slam_dunks => [1, 7, 15, 5, 1]}}
+    }
