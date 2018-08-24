c2eda9a1196f3d9cf39c75d20465e9e3d1149d9f
Daniel Loyd <dhloyd@icloud.com>
2018-08-24 19:47:22 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,7 @@
 def game_hash()
   :home{
     :team_name => {"Brooklyn Nets"},
-    :colors => {"Black", "White"]},
+    :colors => {"Black", "White"},
     :players{:number,
              :shoe,
              :points,
