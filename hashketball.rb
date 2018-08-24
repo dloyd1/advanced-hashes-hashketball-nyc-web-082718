93d630eb233c903cc0c29711149adddf2f7bc20e
Daniel Loyd <dhloyd@icloud.com>
2018-08-24 19:51:06 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,26 +2,21 @@ def game_hash()
   :home{
     :team_name => {"Brooklyn Nets"},
     :colors => {"Black", "White"},
-    :players => {:number,
-             :shoe,
-             :points,
-             :rebounds,
-             :assists,
-             :steals,
-             :blocks,
-             :slam_dunks}
+    :players => {{name: , shoe: , points: , rebounds: , assists: , steals: , blockers: , slam_dunks:},
+                {name: , shoe: , points: , rebounds: , assists: , steals: , blockers: , slam_dunks:},
+                {name: , shoe: , points: , rebounds: , assists: , steals: , blockers: , slam_dunks:},
+                {name: , shoe: , points: , rebounds: , assists: , steals: , blockers: , slam_dunks:},
+                {name: , shoe: , points: , rebounds: , assists: , steals: , blockers: , slam_dunks:}}
 }
 },
   :home{
     :team_name => {"Charlotte Hornets"}
     :colors => {"Turquoise", "Purple"},
-    :players => {name: , shoe: , points: , rebounds: , assists: , steals: , blockers: , slam_dunks:
-             :shoe,
-             :points,
-             :rebounds,
-             :assists,
-             :steals,
-             :blocks,
-             :slam_dunks,
+    :players => {{name: , shoe: , points: , rebounds: , assists: , steals: , blockers: , slam_dunks:},
+                {name: , shoe: , points: , rebounds: , assists: , steals: , blockers: , slam_dunks:},
+                {name: , shoe: , points: , rebounds: , assists: , steals: , blockers: , slam_dunks:},
+                {name: , shoe: , points: , rebounds: , assists: , steals: , blockers: , slam_dunks:},
+                {name: , shoe: , points: , rebounds: , assists: , steals: , blockers: , slam_dunks:}}
+
 }
 },
