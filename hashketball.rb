92f8c3356b832973b1ad509db97021d44c3f9d6d
Daniel Loyd <dhloyd@icloud.com>
2018-08-24 19:58:21 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,11 +4,11 @@ def game_hash()
     :colors => {"Black", "White"},
     :players => {
                   { :name => {["Alan Anderson", "Reggie Evangs", "Brook Lopez", "Mason Plumlee", "Jason Terry"]},
-                    :number => {[0, 30, 11, 1, 31]}
-                    :shoe ,
-                    :points ,
-                    :rebounds ,
-                    :assists ,
+                    :number => [0, 30, 11, 1, 31]
+                    :shoe => [16, 14, 17, 19, 15],
+                    :points => [22, 12, 17, 26, 19],
+                    :rebounds => [12, 12, 19, 12, 2],
+                    :assists => [12, 12, 10, 6, 2],
                     :steals ,
                     :blocks ,
                     :slam_dunks
