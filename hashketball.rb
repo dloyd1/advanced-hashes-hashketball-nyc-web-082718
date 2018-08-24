8b39f1e171770867f6c583cd9670874423a6fb06
Daniel Loyd <dhloyd@icloud.com>
2018-08-24 19:55:24 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,12 +2,18 @@ def game_hash()
   :home{
     :team_name => {"Brooklyn Nets"},
     :colors => {"Black", "White"},
-    :players => {{name: , shoe: , points: , rebounds: , assists: , steals: , blockers: , slam_dunks:},
-                {name: , shoe: , points: , rebounds: , assists: , steals: , blockers: , slam_dunks:},
-                {name: , shoe: , points: , rebounds: , assists: , steals: , blockers: , slam_dunks:},
-                {name: , shoe: , points: , rebounds: , assists: , steals: , blockers: , slam_dunks:},
-                {name: , shoe: , points: , rebounds: , assists: , steals: , blockers: , slam_dunks:}}
-}
+    :players => {
+                  { :name => {["Alan Anderson", "Reggie Evangs", "Brook Lopez", "Mason Plumlee", "Jason Terry"]},
+                    :number => {[0, 30, 11, 1, 31]}
+                    shoe: ,
+                    points: ,
+                    rebounds: ,
+                    assists: ,
+                    steals: ,
+                    blockes: ,
+                    slam_dunks:
+                },
+                }
 },
   :home{
     :team_name => {"Charlotte Hornets"}
