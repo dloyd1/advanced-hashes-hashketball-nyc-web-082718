d95195cd7277e5aa746ec98a74d476fa18ced574
Daniel Loyd <dhloyd@icloud.com>
2018-08-24 19:59:16 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -9,9 +9,9 @@ def game_hash()
                     :points => [22, 12, 17, 26, 19],
                     :rebounds => [12, 12, 19, 12, 2],
                     :assists => [12, 12, 10, 6, 2],
-                    :steals ,
-                    :blocks ,
-                    :slam_dunks
+                    :steals => [3, 12, 3, 3, 4],
+                    :blocks => [1, 12, 1, 8, 11],
+                    :slam_dunks => [1, 7, 15, 5, 1]
                 },
                 }
 },
