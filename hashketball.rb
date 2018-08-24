f6c19a084ca08778e502faa043b184d9e20114e9
Daniel Loyd <dhloyd@icloud.com>
2018-08-24 20:05:13 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -16,13 +16,13 @@ def game_hash()
     :team_name => {"Charlotte Hornets"}
     :colors => {"Turquoise", "Purple"},
     :players => {
-      :name => ["Alan Anderson", "Reggie Evangs", "Brook Lopez", "Mason Plumlee", "Jason Terry"],
-      :number => [0, 30, 11, 1, 31]
-      :shoe => [16, 14, 17, 19, 15],
-      :points => [22, 12, 17, 26, 19],
-      :rebounds => [12, 12, 19, 12, 2],
-      :assists => [12, 12, 10, 6, 2],
+      :name => ["Jeff Adrien", "Bismmak Biyombo", "DeSagna Diop", "Ben Gordon", "Brendan Haywood"],
+      :number => [4, 0, 2, 8, 33]
+      :shoe => [18, 16, 14, 15, 15],
+      :points => [10, 12, 24, 33, 6],
+      :rebounds => [1, 4, 12, 3, 12],
+      :assists => [1, 7, 12, 2, 12],
       :steals => [3, 12, 3, 3, 4],
-      :blocks => [1, 12, 1, 8, 11],
-      :slam_dunks => [1, 7, 15, 5, 1]}}
+      :blocks => [7, 15, 5, 1, 5],
+      :slam_dunks => [2, 10, 5, 0, 12]}}
     }
